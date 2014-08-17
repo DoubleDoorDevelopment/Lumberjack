9b09215e53da701ff46053e514edc5ee5613f6c6
dries007 <admin@dries007.net>
2014-08-17 16:57:45 +0200

    Fix for emerald adding tools

--- src/main/java/net/doubledoordev/timber/items/ItemLumberAxe.java
+++ src/main/java/net/doubledoordev/timber/items/ItemLumberAxe.java
@@ -61,6 +61,7 @@ public class ItemLumberAxe extends ItemAxe
         super(toolMaterial);
 
         String name = toolMaterial.name().toLowerCase();
+        if (toolMaterial == ToolMaterial.EMERALD) name = "diamond";
 
         //Fuck mods that do this: "modid_nameofmaterial"
         if (name.indexOf('_') != -1) name = name.substring(name.indexOf('_') + 1);
